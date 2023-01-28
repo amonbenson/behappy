7b4b73d5c3f2e8d5a873870358e45ebf08fc8120
Schlegel Flegel <amonkbenson@gmail.com>
2023-01-28 20:23:25 +0000

    initial commit

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# install apt packages
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# install miniconda
+wget -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+chmod +x miniconda.sh
+bash miniconda.sh -b
+rm miniconda.sh
+
+# init conda base workspace
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+. ~/.bashrc
+conda init
+conda install -y conda
+
+# create the custom environment file
+conda env update --file environment.yml --prune
+echo "conda activate behappy" >> ~/.bashrc
+. ~/.bashrc
