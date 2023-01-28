13ab8c6076239aa3e73fc50bd74bbb25d18ea039
Schlegel Flegel <amonkbenson@gmail.com>
2023-01-28 16:08:51 +0000

    fixes in setup function

--- setup.sh
+++ setup.sh
@@ -5,15 +5,18 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 # install miniconda
-wget -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
+wget -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x miniconda.sh
-./miniconda.sh -u -b
+bash miniconda.sh -b -u
 rm miniconda.sh
 
 # init conda base workspace
-eval "$($HOME/gitpod/miniconda3/bin/conda shell.bash hook)"
+eval "$($HOME/miniconda3/bin/conda shell.bash hook)"
+. ~/.bashrc
 conda init
+conda install -y conda
 
 # create the custom environment file
-conda env create -f environment.yml --force
-conda activate behappy
+conda env update --file environment.yml --prune
+echo "conda activate behappy" >> ~/.bashrc
+. ~/.bashrc
