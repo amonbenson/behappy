e24ad474f1e7184584b53ab17bad91d1e42fbbe7
Schlegel Flegel <amonkbenson@gmail.com>
2023-01-28 15:46:35 +0000

    reinit

--- .gitpod.Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM gitpod/workspace-base
-
-USER gitpod
-
-RUN wget -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-py310_22.11.1-1-Linux-x86_64.sh \
-    && chmod +x miniconda.sh \
-    && ./miniconda.sh -u -b \
-    rm miniconda.sh
--- setup.sh
+++ setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 # install apt packages
-sudo apt-get update && sudo apt-get upgrade
+sudo apt-get update
+sudo apt-get upgrade -y
 
 # install miniconda
-wget -O - https://repo.anaconda.com/miniconda/Miniconda3-py310_22.11.1-1-Linux-x86_64.sh | bash -s - -b -u
+wget -O miniconda.sh https://repo.anaconda.com/miniconda/Miniconda3-py310_22.11.1-1-Linux-x86_64.sh
+chmod +x miniconda.sh
+./miniconda.sh -u -b
+rm miniconda.sh
 
 # init conda base workspace
 eval "$($HOME/gitpod/miniconda3/bin/conda shell.bash hook)"
